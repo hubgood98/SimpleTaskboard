d4ca80f10e69ca48feb8d9ed758dc15de1ce68b5
huijun98 <heej1998@gmail.com>
2024-05-22 19:59:28 +0900

    getMapping 방식 작성

---
+++ src/main/java/org/study/study240522/controller/helloController.java
@@ -0,0 +1,25 @@
+package org.study.study240522.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class helloController {
+
+    @GetMapping("hello")
+    public String hello(Model model)
+    {
+        model.addAttribute("msg", "Hello World");
+
+        return "hello";
+    }
+
+    @GetMapping("hello-MVC")
+    public String helloMVC(@RequestParam("name")String name, Model model)
+    {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}

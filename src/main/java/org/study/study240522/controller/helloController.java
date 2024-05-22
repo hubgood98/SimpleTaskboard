74a68e78b18049a58078917621275b784d3686f2
huijun98 <heej1998@gmail.com>
2024-05-22 20:40:21 +0900

    .

--- src/main/java/org/study/study240522/controller/helloController.java
+++ src/main/java/org/study/study240522/controller/helloController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class helloController {
@@ -24,11 +23,4 @@ public class helloController {
         return "hello";
     }
 
-    @GetMapping("hello-string")
-    public String helloString(@RequestParam("name") String name, Model model)
-    {
-        model.addAttribute("name", name);
-
-        return "hello_"+name;
-    }
 }

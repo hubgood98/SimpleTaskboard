b0dae5a43592a5cde6b2941a458979747f5bc6d6
huijun98 <heej1998@gmail.com>
2024-05-22 19:59:35 +0900

    .

--- src/main/java/org/study/study240522/Study240522Application.java
+++ src/main/java/org/study/study240522/Study240522Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Study240522Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Study240522Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Study240522Application.class, args);
+    }
 
 }
